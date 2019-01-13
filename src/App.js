86f3584a4d8922ead16fbc98f7ab08d3db73a399
t-h-e-r-o-x <31180828+t-h-e-r-o-x@users.noreply.github.com>
2019-01-14 00:17:37 +0530

    WelcomeFrontEnd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,143 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Signin from './Components/Signin/Signin';
+import Register from './Components/Register/Register';
+import Particles from 'react-particles-js';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import Clarifai from 'clarifai';
+import FaceRecog from './Components/FaceRecog/FaceRecog';
+
+
+
+
+const particlesOptions={
+            		particles: {
+                  number:{
+                    value: 80,
+                    density:{
+                      enable: true,
+                      value_area: 800
+                    }
+                  }
+            	}
+            }
+
+const initialState = {
+  input: ' ',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user:{
+    id:'',
+    name:'',
+    email:'',
+    password:'',
+    entries: 0,
+    joined: ''
+  }
+}
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = initialState;
+  }
+
+loadUser = (data) => {
+  this.setState({user:{
+    id:data.id,
+    name:data.name,
+    email:data.email,
+    password:data.password,
+    entries: data.entries,
+    joined: data.joined
+  }})
+}
+
+ calculateFaceLocation = (data) => {
+   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+   const image = document.getElementById('inputimage');
+   const width= Number(image.width);
+   const height= Number(image.height);
+   return{
+     leftCol: clarifaiFace.left_col * width,
+     topRow: clarifaiFace.top_row * height,
+     rightCol: width - (clarifaiFace.right_col * width),
+     bottomRow: height - (clarifaiFace.bottom_row * height)
+   }
+}
+
+displayFaceBox = (box) => {
+  this.setState({ box : box});
+}
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    fetch('https://agile-basin-17843.herokuapp.com/imageUrl', {
+        method:'post',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(response => response.json())
+    .then((response) => {
+      if(response){
+        fetch('https://agile-basin-17843.herokuapp.com/image', {
+            method:'put',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+        .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count }))
+          })
+          .catch(console.log);
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))})
+    .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if(route==='signout'){
+      this.setState(initialState);
+    }
+    else if( route==='home'){
+      this.setState({isSignedIn: true});
+    }
+  this.setState({route: route});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Particles className="particles" params={particlesOptions}/>
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+        {this.state.route === 'home' ?
+        <div>
+         <Logo />
+         <Rank name={this.state.user.name} entries={this.state.user.entries} />
+         <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
+         <FaceRecog box={this.state.box} imageUrl={this.state.imageUrl}/>
+       </div>
+        :(
+          this.state.route === 'signin'?
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+          :
+          <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+        )
+
+    }
+  </div>
     );
   }
 }
